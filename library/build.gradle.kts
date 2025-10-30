91fc8101c80553a41cc627fbf8cc4eb6d6a35ead
Joel Roman <joelrsosarivera@gmail.com>
2025-10-30 17:55:46 -0400

    ci: publishing updates

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -27,11 +27,36 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.joelromanpr",
         artifactId = "commandline-ktx",
-        version = "1.0.0"
+        version = version.toString()
     )
 
     pom {
         name.set("commandline-ktx")
         description.set("A simple, modern, and type-safe command-line argument parser for Kotlin.")
+        url.set("https://github.com/joelromanpr/commandline-ktx")
+
+        licenses {
+            license {
+                name.set("The MIT License")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("joelromanpr")
+                name.set("Joel Roman")
+                email.set("contact@joelromanpr.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/joelromanpr/commandline-ktx")
+            connection.set("scm:git:git://github.com/joelromanpr/commandline-ktx.git")
+            developerConnection.set("scm:git:ssh://git@github.com/joelromanpr/commandline-ktx.git")
+        }
     }
+
+    // Configure signing for all publications
+    signAllPublications()
 }
